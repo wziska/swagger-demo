65439a8d5bbcf7fec18f6ba9acdf8b155865918f
Wojciech Ziska <woza@demant-technology.com>
2019-02-07 17:54:48 +0100

    naive implementation

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -10,36 +10,42 @@ namespace TodoApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
+        private static IList<string> values = new List<string> { "value1", "value2" };
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return values.ToArray();
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return values[id];
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            values.Add(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            values[id] = value;
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            values.RemoveAt(id);
         }
     }
 }
